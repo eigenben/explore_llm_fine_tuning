6409d08011e01c8e431083fd6a66c606b600d454
Ben Hughes <me@benhughes.name>
2025-12-18 12:32:38 -0800

    Add example for training llama3 with GRPO on gsm8k via unsloth

---
+++ unsloth/train_llama3_grpo.py
@@ -0,0 +1,195 @@
+import torch
+import re
+from datasets import load_dataset, Dataset
+from unsloth import FastLanguageModel
+from trl import GRPOConfig, GRPOTrainer
+from vllm import SamplingParams
+
+# Setup some parameters
+MAX_SEQ_LENGTH = 512
+MAX_PROMPT_LENGTH = 256
+MODEL_NAME = "unsloth/meta-Llama-3.1-8B-Instruct"
+ADAPTER_NAME = "llama-3.1-8b-grpo-gsm8k-finetuned"
+LORA_RANK = 32
+
+# Setup the Model:
+model, tokenizer = FastLanguageModel.from_pretrained(
+    MODEL_NAME,
+    max_seq_length = MAX_SEQ_LENGTH,
+    load_in_4bit = True,
+    fast_inference = True,
+    max_lora_rank = LORA_RANK,
+    gpu_memory_utilization = 0.8,
+    dtype = torch.float16,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = LORA_RANK,
+    target_modules = [
+        "q_proj", "k_proj", "v_proj", "o_proj",
+        "gate_proj", "up_proj", "down_proj",
+    ], # Remove QKVO if out of memory
+    lora_alpha = LORA_RANK,
+    use_gradient_checkpointing = "unsloth", # Enable long context finetuning
+    random_state = 3407,
+)
+
+# Prepare data:
+SYSTEM_PROMPT = """
+Respond in the following format:
+<reasoning>
+...
+</reasoning>
+<answer>
+...
+</answer>
+"""
+
+XML_COT_FORMAT = """\
+<reasoning>
+{reasoning}
+</reasoning>
+<answer>
+{answer}
+</answer>
+"""
+
+def extract_xml_answer(text: str) -> str:
+    answer = text.split("<answer>")[-1]
+    answer = answer.split("</answer>")[0]
+    return answer.strip()
+
+def extract_hash_answer(text: str) -> str | None:
+    if "####" not in text:
+        return None
+    return text.split("####")[1].strip()
+
+# uncomment middle messages for 1-shot prompting
+def get_gsm8k_questions(split = "train") -> Dataset:
+    data = load_dataset('openai/gsm8k', 'main')[split] # type: ignore
+    data = data.map(lambda x: { # type: ignore
+        'prompt': [
+            {'role': 'system', 'content': SYSTEM_PROMPT},
+            {'role': 'user', 'content': x['question']}
+        ],
+        'answer': extract_hash_answer(x['answer'])
+    }) # type: ignore
+    return data # type: ignore
+
+dataset = get_gsm8k_questions()
+print("Columns in Dataset: ", dataset.column_names)
+
+# Setup Reward functions
+def correctness_reward_func(prompts, completions, answer, **kwargs) -> list[float]:
+    responses = [completion[0]['content'] for completion in completions]
+    q = prompts[0][-1]['content']
+    extracted_responses = [extract_xml_answer(r) for r in responses]
+    print('-'*20, f"Question:\n{q}", f"\nAnswer:\n{answer[0]}", f"\nResponse:\n{responses[0]}", f"\nExtracted:\n{extracted_responses[0]}")
+    return [2.0 if r == a else 0.0 for r, a in zip(extracted_responses, answer)]
+
+def int_reward_func(completions, **kwargs) -> list[float]:
+    responses = [completion[0]['content'] for completion in completions]
+    extracted_responses = [extract_xml_answer(r) for r in responses]
+    return [0.5 if r.isdigit() else 0.0 for r in extracted_responses]
+
+def strict_format_reward_func(completions, **kwargs) -> list[float]:
+    """Reward function that checks if the completion has a specific format."""
+    pattern = r"^<reasoning>\n.*?\n</reasoning>\n<answer>\n.*?\n</answer>\n$"
+    responses = [completion[0]["content"] for completion in completions]
+    matches = [re.match(pattern, r) for r in responses]
+    return [0.5 if match else 0.0 for match in matches]
+
+def soft_format_reward_func(completions, **kwargs) -> list[float]:
+    """Reward function that checks if the completion has a specific format."""
+    pattern = r"<reasoning>.*?</reasoning>\s*<answer>.*?</answer>"
+    responses = [completion[0]["content"] for completion in completions]
+    matches = [re.match(pattern, r) for r in responses]
+    return [0.5 if match else 0.0 for match in matches]
+
+def count_xml(text) -> float:
+    count = 0.0
+    if text.count("<reasoning>\n") == 1:
+        count += 0.125
+    if text.count("\n</reasoning>\n") == 1:
+        count += 0.125
+    if text.count("\n<answer>\n") == 1:
+        count += 0.125
+        count -= len(text.split("\n</answer>\n")[-1])*0.001
+    if text.count("\n</answer>") == 1:
+        count += 0.125
+        count -= (len(text.split("\n</answer>")[-1]) - 1)*0.001
+    return count
+
+def xmlcount_reward_func(completions, **kwargs) -> list[float]:
+    contents = [completion[0]["content"] for completion in completions]
+    return [count_xml(c) for c in contents]
+
+
+# Setup Trainer:
+training_args = GRPOConfig(
+    learning_rate = 5e-6,
+    adam_beta1 = 0.9,
+    adam_beta2 = 0.99,
+    weight_decay = 0.1,
+    warmup_ratio = 0.1,
+    lr_scheduler_type = "cosine",
+    optim = "paged_adamw_8bit",
+    logging_steps = 1,
+    per_device_train_batch_size = 1,
+    gradient_accumulation_steps = 1, # Increase to 4 for smoother training
+    num_generations = 6, # Decrease if out of memory
+    max_prompt_length = MAX_PROMPT_LENGTH,
+    max_completion_length = MAX_SEQ_LENGTH - MAX_PROMPT_LENGTH,
+    # num_train_epochs = 1, # Set to 1 for a full training run
+    max_steps = 500,
+    save_steps = 500,
+    max_grad_norm = 0.1,
+    report_to = "wandb", # Can use Weights & Biases
+    output_dir = "outputs",
+)
+
+trainer = GRPOTrainer(
+    model = model,
+    processing_class = tokenizer,
+    reward_funcs = [
+        xmlcount_reward_func,
+        soft_format_reward_func,
+        strict_format_reward_func,
+        int_reward_func,
+        correctness_reward_func,
+    ],
+    args = training_args,
+    train_dataset = dataset,
+)
+
+# Show memory usage before training
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+# Train the Model:
+trainer_stats = trainer.train()
+
+# Show memory usage after training
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory / max_memory * 100, 3)
+lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training.")
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+# Save the Model:
+model.save_pretrained(ADAPTER_NAME)
+tokenizer.save_pretrained(ADAPTER_NAME)
+model.push_to_hub(ADAPTER_NAME)
+tokenizer.push_to_hub(ADAPTER_NAME)
+
+model.save_pretrained_gguf(ADAPTER_NAME + "-gguf", tokenizer, quantization_method="q4_k_m")
+model.push_to_hub_gguf(ADAPTER_NAME + "-gguf", tokenizer, quantization_method="q4_k_m", token="")
